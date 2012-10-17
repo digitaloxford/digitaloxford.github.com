b196e9b41dcf6cb9f8be4d317888d9b0b633092c
carlwood <cjwood17@googlemail.com>
2012-10-17 17:34:49 +0100

    Various re-workings and fixes.

--- js/script.js
+++ js/script.js
@@ -21,23 +21,55 @@ $(function(){
 
         scrollToDiv(elWrapped,40);
 
-        $(elWrapped).find('.hidden').slideDown().siblings('.more-link').find('a').addClass('expanded');
+        $(elWrapped).find('.hidden-post').slideDown().siblings('.more-link').find('a').addClass('expanded');
+
+        // Toggle text for more details / expandable links
+
+        var moreEvents = $('#events .more-link a');
+
+        if($(moreEvents).hasClass('expanded')) {
+            $(moreEvents).text('Fewer events');
+        } else {
+            $(moreEvents).text('More events');
+        }
+
+        var moreJobs = $('#jobs .more-link a');
+
+        if($(moreJobs).hasClass('expanded')) {
+            $(moreJobs).text('Fewer jobs');
+        } else {
+            $(moreJobs).text('More jobs');
+        }
+
 
         e.preventDefault();
     });
 
     /* Expand / hide sections */
 
-    $('.more-link').on('click', 'a', function(e){
+    $('.more-link').on('click', 'a.expand', function(e){
         e.preventDefault();
-        var hey = $(this).parents('.more-link').siblings('.hidden').slideToggle();
-
+        var hey = $(this).parents('.more-link').siblings('.hidden-post').slideToggle();
         $(this).toggleClass('expanded');
+    });
+
+
+
+    $('#events .more-link').on('click', 'a', function(e){
+        if($(this).hasClass('expanded')) {
+            $(this).text('Fewer events');
+        } else {
+            $(this).text('More events');
+        }
 
     });
 
     $('#jobs .more-link').on('click', 'a', function(e){
-
+        if($(this).hasClass('expanded')) {
+            $(this).text('Fewer jobs');
+        } else {
+            $(this).text('More jobs');
+        }
 
     });
 
