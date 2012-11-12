05702a138d0ba92621af1a0fe0059785c348f5d1
Martin Wright <martin.wright@torchbox.com>
2012-11-12 16:49:16 +0000

    removed tweets from footer and JS

--- js/script.js
+++ js/script.js
@@ -74,31 +74,5 @@ $(function(){
     });
 
 
-    /* Twitter feed */
-
-    $.getJSON('http://search.twitter.com/search.json?q=from:digitaloxford&callback=?')
-    .pipe(function(data){
-        return (data.results || []).slice(0,3)
-    })
-    .then(function(tweets){
-        var tweetlis = $.map(tweets, function(tweet){
-            var $li = $('<li class="col">').text(tweet.text).append(' ');
-
-            var d = new Date(tweet.created_at);
-
-            var day = d.getDate();
-            var month = "J F M A M J J A S October N D".split(' ')[d.getMonth()];
-            var year = d.getFullYear();
-
-            var $a = $('<a>',{href:'https://twitter.com/digitaloxford/status/' + tweet.id_str})
-                        .text(day + ' ' + month + ' ' + year);// todo - format date
-
-            $a.appendTo($li);
-            return $li;
-        });
-
-        $('#tweet-panel').empty().append(tweetlis);
-    });
-
-
+   
 });
